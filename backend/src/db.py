d41a9bc05fab3c654f02667e2cae080efb88052a
JohnRuben717 <pjohn21@karunya.edu.in>
2024-12-20 13:16:48 +0530

    updates

--- backend/src/db.py
+++ backend/src/db.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy.ext.declarative import declarative_base
 
-DATABASE_URL = "postgresql://postgres:1117@localhost:5432/todo_app"
-# DATABASE_URL = "postgresql://rchat_user:n7JNecTQKuBL1hgXXKGoviE1hedQ7trX@dpg-cti5hh3tq21c73a0tmc0-a.singapore-postgres.render.com/rchat"
+# DATABASE_URL = "postgresql://postgres:1117@localhost:5432/todo_app"
+DATABASE_URL = "postgresql://rchat_user:n7JNecTQKuBL1hgXXKGoviE1hedQ7trX@dpg-cti5hh3tq21c73a0tmc0-a.singapore-postgres.render.com/rchat"
 
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
